1f49577690bc1383f22c976e2c950626b7355ed8
sarvajeetsingh007 <sarvajeetsingh007@gmail.com>
2015-03-16 12:10:56 +0530

    initial

---
+++ AppWarp_S2_Server/src/demo/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+
+
+import com.shephertz.app42.server.AppWarpServer;
+
+/**
+ * @author DHRUV CHOPRA
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+	String appconfigPath = System.getProperty("user.dir")+System.getProperty("file.separator")+"AppConfig.json";
+	boolean started = AppWarpServer.start(new RummyServerExtension(), appconfigPath);
+        if(!started){
+            throw new Exception("AppWarpServer did not start. See logs for details.");
+        }
+    }
+
+}
